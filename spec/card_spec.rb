79831d42dab23c2ae7dd1e0f627a43ab0eefd5cd
Mark Harmsen <mark.harmsen@woperty.com>
2019-06-21 16:25:04 -0400

    The expect and eq methods

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,4 +1,11 @@
 Rspec.describe 'Card' do
   it 'has a type' do
+    card = Card.new('Ace of Spades')
+
+    expect(card.type).to eq('Ace of Spades')
+  end
+
+  it 'this is a example' do
+    expect(1 + 1).to eq(2)
   end
 end
