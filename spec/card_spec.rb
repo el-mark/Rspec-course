92c89a1fc7b12eed15de30ad50bdc7677459cfcb
Mark Harmsen <mark.harmsen@woperty.com>
2019-06-28 16:17:58 -0400

    Hooks and instances

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,6 +1,5 @@
 class Card
-  attr_accessor :rank
-  attr_accessor :suit
+  attr_accessor :rank, :suit
   def initialize(rank, suit)
     @rank = rank
     @suit = suit
@@ -8,17 +7,15 @@ class Card
 end
 
 RSpec.describe Card do
-  it 'has a rank' do
-    card = Card.new('Ace', 'Spades')
-    expect(card.rank).to eq('Ace')
+  before do 
+    @card = Card.new('Ace', 'Spades')
+  end
+
+  it 'has a rank' do  
+    expect(@card.rank).to eq('Ace')
   end
 
   it 'has a suit' do
-    card = Card.new('Ace', 'Spades')
-    expect(card.suit).to eq('Spades')
-  end
-
-  it 'this is a example' do
-    expect(1 + 1).to eq(2)
+    expect(@card.suit).to eq('Spades')
   end
 end
