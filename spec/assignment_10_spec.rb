db4401a84534ca7476331bba32476f24132821ce
Mark <mharmsenr@gmail.com>
2019-08-15 18:19:13 -0400

    Addignment 10

--- spec/assignment_10_spec.rb
+++ spec/assignment_10_spec.rb
@@ -1,7 +1,13 @@
 RSpec.describe 'doubles' do
   it "tests the student's knowledge of the course's content" do
  
-    # For each question, write the code that should go here
- 
+    db = double("Database Connection", connect: true, disconnect: 'Goodbye')
+
+    expect(db.connect).to eq(true)
+    expect(db.disconnect).to eq('Goodbye')
+
+    fs = double("File System")
+    allow(fs).to receive_messages(read: 'Romeo and Juliet')
+    allow(fs).to receive_messages(write: false)
   end
 end
